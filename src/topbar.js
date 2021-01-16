00672cea30aa7bdb1aee3fa8b0e3b161ac2bfa26
luketk97 <44345212+luketk97@users.noreply.github.com>
2021-01-16 14:04:37 -0700

    simple topbar (#5)

--- src/topbar.js
+++ src/topbar.js
@@ -1,3 +1,23 @@
 import React from 'react'
 import {Appbar, Toolbar, IconButton, Typography, Button} from '@material-ui/core';
+import {Menu} from '@material-ui/icons'
 
+function topbar(){
+    return (
+        <div>
+            <Appbar position='static'>
+                <Toolbar>
+                    <Typography>
+                    SimplyScheduler
+                    </Typography>
+                    <IconButton>
+                    <Menu/>
+                    </IconButton>
+                    <Button>Login</Button>
+                </Toolbar>
+            </Appbar>
+        </div>
+    );
+}
+
+export default topbar;
